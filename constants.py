7266a6eceac05cdec4f10547dc2aff874854626b
Cheesecaketv53 <cheesecaketv53@gmail.com>
2026-02-03 12:31:13 +0100

    Added nato alphabet-constant

--- constants.py
+++ constants.py
@@ -39,6 +39,35 @@ LETTERS = {
 
 LETTERS = {key:val.replace(".", SHORT).replace("-", LONG) for key,val in LETTERS.items()}
 
+NATO_ALPHABET = {
+    "A": "ALFA",
+    "B": "BRAVO",
+    "C": "CHARLIE",
+    "D": "DELTA",
+    "E": "ECHO",
+    "F": "FOXTROT",
+    "G": "GOLF",
+    "H": "HOTEL",
+    "I": "INDIA",
+    "J": "JULIETT",
+    "K": "KILO",
+    "L": "LIMA",
+    "M": "MIKE",
+    "N": "NOVEMBER",
+    "O": "OSCAR",
+    "P": "PAPA",
+    "Q": "QUEBEC",
+    "R": "ROMEO",
+    "S": "SIERRA",
+    "T": "TANGO",
+    "U": "UNIFORM",
+    "V": "VICTOR",
+    "W": "WHISKEY",
+    "X": "XRAY",
+    "Y": "YANKEE",
+    "Z": "ZULU",
+}
+
 # Colors
 GREEN = sg.Color.SpringGreen4
 RED = sg.Color.OrangeRed3
